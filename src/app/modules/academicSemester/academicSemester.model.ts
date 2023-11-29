e69cbfd601a51236e23993dee701eea082b17581
byomkesh2580 <habibthemerchant@gmail.com>
2023-11-29 12:08:36 +0600

    academicsemesterStarted

---
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -0,0 +1,45 @@
+import { Schema, model } from 'mongoose';
+import {
+  AcademicSemesterCode,
+  AcademicSemesterName,
+  Months,
+} from './academicSemester.constant';
+import { TAcademicSemseter } from './academicSemester.interface';
+
+
+const acdemicSemesterSchema = new Schema<TAcademicSemseter>(
+  {
+    name: {
+      type: String,
+      required: true,
+      enum: AcademicSemesterName,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+      enum: AcademicSemesterCode,
+    },
+    startMonth: {
+      type: String,
+      required: true,
+      enum: Months,
+    },
+    endMonth: {
+      type: String,
+      required: true,
+      enum: Months,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const AcademicSemester = model<TAcademicSemseter>(
+  'AcademicSemester',
+  acdemicSemesterSchema,
+);
