13660c0b95c1fe750df9a050cfad1d84512a26ec
byomkesh2580 <habibthemerchant@gmail.com>
2023-11-29 10:58:42 +0600

    ontheWay

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/v1', router);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world, running');
+});
+
+app.use(globalErrorHandler);
+
+//Not Found
+app.use(notFound);
+
+export default app;
---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,21 @@
+import { userServices } from "./user.service";
+import sendResponse from "../../utils/sendResponse";
+import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
+
+
+const createStudent = catchAsync(async (req, res) => {
+    const { password, student: studentData } = req.body;
+    const result = await userServices.createStudentIntoDB(password, studentData);
+        
+    sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is created succesfully',
+    data: result,
+  });
+})
+
+export const UserControllers = {
+  createStudent,
+};
